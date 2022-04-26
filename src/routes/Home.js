3fdbf538b5e1f1624f936a434c6b0106dfbd2506
seonu@yanadoocorp.com <seonu@yanadoocorp.com>
2022-04-26 23:58:47 +0900

    Movies Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,25 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(loading);
+  if (loading) {
+    return 'loading...';
+  }
+
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1 key={m.id}>{m.id}</h1>);
+  }
+  return <h1>Home</h1>;
+};
