7f182a9be4a1c947a11397a74ff735a84200f01c
seonu@yanadoocorp.com <seonu@yanadoocorp.com>
2022-04-28 01:22:23 +0900

    deatil getMovie

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  // uri: "http://localhost:4000",
-  uri: 'https://movieql2.vercel.app/',
+  uri: 'http://localhost:4000',
+  // uri: 'https://movieql2.vercel.app/',
   cache: new InMemoryCache(),
 });
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import client from './apollo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
