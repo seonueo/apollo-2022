6ec75ff9d704bc4f0c2cc76854047f19999f8d26
seonu@yanadoocorp.com <seonu@yanadoocorp.com>
2022-04-28 23:13:29 +0900

    movie styled & detail update

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,16 +2,61 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+  width: 50%;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: 50% 50%;
 `;
 
 export default () => {
@@ -19,10 +64,23 @@ export default () => {
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: +id },
   });
-  if (loading) {
-    return 'loading';
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading' : data.movie.title}</Title>
+        <Subtitle>
+          {data?.movie?.language} &middot; {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
+      {/* {data &&
+        data.suggestions &&
+        data.suggestions.map((s) => (
+          <div>
+            <img src={s.medium_cover_image} />
+          </div>
+        ))} */}
+    </Container>
+  );
 };
